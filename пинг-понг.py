ae03a69d352beaa5bdb28a3d6f06d38749a5dcc7
holodilnik <138233273+holodilnik9865@users.noreply.github.com>
2023-07-01 13:30:19 +0300

    пинг-понг.py

--- пинг-понг.py
+++ пинг-понг.py
@@ -3,14 +3,14 @@ from pygame import *
 
 class GameSprite(sprite.Spite):
 	def __init__(self, player_image, player_x, player_y, player_speed, wight, height):
-	super().__init__()
-	self.image = transform.scale(image.load(player_image), (wight, height))
-	self.speed = player_speed
-	self.rect = self.image.get_rect()
-	self.rect.x = player_x
-	self.rect.y = player_y
+	    super().__init__()
+	    self.image = transform.scale(image.load(player_image), (wight, height))
+	    self.speed = player_speed
+	    self.rect = self.image.get_rect()
+	    self.rect.x = player_x
+	    self.rect.y = player_y
 
-	def reset)self):
+	def reset(self):
 		window.blit(self.image, (self.rect.x, self.rect.y))
 
 class Player(GameSprite):
@@ -19,11 +19,12 @@ class Player(GameSprite):
 		if keys[K_UP] and self.rect.y > 5:
 			self.rect.y -= self.speed
 		if keys[K_DOWN] and self.rect.y < win_height - 80:
+            		self.rect.y += self.speed
 	def update_l(self):
-		keys = keys.get_pressed()
-		if key[K_w] and self.rect.y > 5:
-			self.rect.y -= self.speed
-		if keys[K_s] and self.rect.y < win_height - 80:
+        	keys = key.get_pressed()
+        	if keys[K_w] and self.rect.y > 5:
+		    	self.rect.y -= self.speed
+	   	if keys[K_s] and self.rect.y < win_height - 80:
 			self.rect.y += self.speed
 
 back = (200, 255, 255)
